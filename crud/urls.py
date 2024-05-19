fa7e8609fe242e4a34fd0118056f774abb558cf1
AlfurIvan <zbsrebyata@gmail.com>
2024-05-19 17:15:50 +0300

    Finished simple crud + list(filtered) crud

--- crud/urls.py
+++ crud/urls.py
@@ -1,7 +1,7 @@
 
 from django.urls import path
 from .views import (
-    ListDatabases, ListCollections, ListDocuments, DocumentDetail, AddDocument, AddDocuments
+    ListDatabases, ListCollections, ListDocuments, DocumentDetail, CreateDocument
 )
 
 urlpatterns = [
@@ -9,6 +9,6 @@ urlpatterns = [
     path('databases/collections/', ListCollections.as_view(), name='list-collections'),
     path('databases/collections/<str:collection_name>/documents/', ListDocuments.as_view(), name='list-documents'),
     path('databases/collections/<str:collection_name>/documents/<str:document_id>/', DocumentDetail.as_view(), name='document-detail'),
-    path('databases/collections/<str:collection_name>/documents/add/', AddDocument.as_view(), name='add-document'),
-    path('databases/collections/<str:collection_name>/documents/add-many/', AddDocuments.as_view(), name='add-documents'),
+    path('databases/collections/<str:collection_name>/documents-add/', CreateDocument.as_view(), name='add-document-s'),
+
 ]
--- crud/views.py
+++ crud/views.py
@@ -1,3 +1,4 @@
+import json
 from rest_framework import status, generics
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -21,13 +22,38 @@ class ListCollections(APIView):
 
 
 class ListDocuments(APIView):
+
+    def get_filters(self, request):
+        try:
+            filters_str = request.query_params.dict()["filter"]
+            filters = json.loads(filters_str)
+        except KeyError:
+            filters = {}
+        return filters
+
     def get(self, request, collection_name):
-        filters = request.query_params.dict()
+        filters = self.get_filters(request)
         documents = client.list_documents(collection_name, filters)
         for doc in documents:
             doc['_id'] = str(doc['_id'])
         return Response(documents)
 
+    def put(self, request, collection_name):
+        filters = self.get_filters(request)
+        serializer = RoadmapSerializer(data=request.data)
+        if serializer.is_valid():
+            update = serializer.validated_data
+            matched, modified = client.update_documents(collection_name, filters, update)
+            return Response({'matched': matched, 'modified': modified})
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, collection_name):
+        filters = self.get_filters(request)
+
+        if not filters:
+            return Response({"message":"I dont want to set the world on fire"})
+        deleted_count = client.delete_documents(collection_name, filters)
+        return Response({'deleted_count': deleted_count})
 
 class DocumentDetail(APIView):
     def get(self, request, collection_name, document_id):
@@ -52,18 +78,22 @@ class DocumentDetail(APIView):
         return Response({'deleted_count': deleted_count})
 
 
-class AddDocument(generics.CreateAPIView):
-    serializer_class = RoadmapSerializer
+class CreateDocument(APIView):
 
-    def perform_create(self, serializer):
-        collection_name = self.kwargs['collection_name']
-        client.add_document(collection_name, serializer.validated_data)
+    def get(self,request, *args, **kwargs):
+        return Response(data={"message":"debug"})
 
-
-class AddDocuments(APIView):
     def post(self, request, collection_name):
-        serializer = RoadmapSerializer(data=request.data, many=True)
+        if isinstance(request.data, list):  # Перевіряємо, чи надійшов список документів
+            serializer = RoadmapSerializer(data=request.data, many=True)
+        else:
+            serializer = RoadmapSerializer(data=request.data)
+
         if serializer.is_valid():
-            inserted_ids = client.add_documents(collection_name, serializer.validated_data)
+            if isinstance(request.data, list):  # Якщо надійшов список документів
+                inserted_ids = client.add_documents(collection_name, serializer.validated_data)
+            else:
+                inserted_ids = [client.add_document(collection_name, serializer.validated_data)]
+
             return Response({'inserted_ids': inserted_ids}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
